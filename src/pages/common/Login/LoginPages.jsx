cf7c697b15d047125e35b5db8081e09cdc623945
김형관 <bearson0115@gmail.com>
2025-06-30 15:46:05 +0900

    2025_06_30_1436

---
+++ src/pages/common/Login/LoginPages.jsx
@@ -0,0 +1,10 @@
+const LoginPages = () => {
+
+    return(
+        <div>
+            <a href="http://localhost:8080/oauth/kakao/callback"></a>
+        </div>
+    )
+}
+
+export default LoginPages

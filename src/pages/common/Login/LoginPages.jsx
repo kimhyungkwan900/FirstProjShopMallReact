e258c7d3eda08b58e8989d37a188f51f9726450d
김형관 <bearson0115@gmail.com>
2025-06-30 17:50:12 +0900

    2025_06_30_1750

--- src/pages/common/Login/LoginPages.jsx
+++ src/pages/common/Login/LoginPages.jsx
@@ -1,8 +1,13 @@
+import kakaoLoginImg from "../../../assets/Kakao/ko/kakao_login_medium_narrow.png"
+
 const LoginPages = () => {
 
     return(
         <div>
-            <a href="http://localhost:8080/oauth/kakao/callback"></a>
+            <h1>로그인페이지</h1>
+            <a href="http://localhost:8080/login/oauth2/code/kakao">
+                <img src={kakaoLoginImg} alt="" />
+            </a>
         </div>
     )
 }

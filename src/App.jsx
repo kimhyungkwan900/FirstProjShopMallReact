1deb96efd62c4c55b53f4ee9442614a640180505
kim Gun ho <vlfh0002@gmail.com>
2025-07-02 09:39:16 +0900

    충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,4 @@
 import './App.css'
-<<<<<<< HEAD
-import ReviewButton from './component/user/review/ReviewButton'
-import { RouterProvider } from 'react-router-dom'
-import ReviewRouter from './router/review/ReviewRouter'
-function App() {
-
-  return (
-    <>
-      <ReviewButton productId={1}/>
-      <RouterProvider router = {ReviewRouter}/>
-    </>
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductListPage from './pages/user/product/ProductListPage';
 import NotFoundPage from './pages/user/product/NotFoundPage';
@@ -24,7 +12,6 @@ function App() {
         <Route path="*" element={<NotFoundPage />} /> {/* 옵션 */}
       </Routes>
     </Router>
->>>>>>> main
   )
 }
 

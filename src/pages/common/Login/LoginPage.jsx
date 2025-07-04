9bca75bbc39dd62cfe12e1ba68ccbedb9c1f4011
김형관 <bearson0115@gmail.com>
2025-07-04 09:49:40 +0900

    2025_07_04_0949

--- src/pages/common/Login/LoginPage.jsx
+++ src/pages/common/Login/LoginPage.jsx
@@ -2,11 +2,21 @@ import kakaoLoginImg from "../../../assets/Kakao/ko/kakao_login_large_narrow.png
 import naverLoginImg from "../../../assets/Naver/2021_Login_with_naver_guidelines_Kr/btnD_아이콘원형.png";
 import googleLoginImg from "../../../assets/Google/signin-assets/Web (mobile + desktop)/png@1x/dark/web_dark_rd_na@1x.png";
 import LoginForm from "./LoginForm";
+import axios from "axios";
 const LoginPage = () => {
+    
+    const onLogin = async (userId, password) => {
+        try{
+            const response = await axios.post("http://localhost:8080/api/login", {userId, password});
+            console.log("쇼핑몰에 오신걸 환영합니다.", response.data);
+        } catch (e) {
+            console.e("ID 또는 비밀번호를 확인해주세요", e.response?.data || e.message);
+        }
+    }
 
     return(
         <div className="max-w-sm mx-auto mt-10 p-6 bg-white rounded-xl shadow-md">
-            <LoginForm />
+            <LoginForm onLogin={onLogin}/>
             <div className="flex justify-center gap-6 mt-3 items-center">
                 <a href="http://localhost:8080/login/oauth2/code/kakao">
                     <img src={kakaoLoginImg} alt="카카오 로그인" className="w-12 h-12 object-cover rounded-full shadow-md"/>

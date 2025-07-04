8e290826ce18e04c0bb972521bb7c17e36139a26
김형관 <bearson0115@gmail.com>
2025-07-04 09:49:40 +0900

    2025_07_04_0949

--- src/pages/common/Login/SignUpPage.jsx
+++ src/pages/common/Login/SignUpPage.jsx
@@ -1,11 +1,20 @@
+import axios from "axios";
 import SignUpForm from "./SignUpForm";
 
 const SignUpPage = () => {
 
+    const onSignUp = async (form) =>{
+        try {
+            const response = await axios.post("http://localhost:8080/api/signup",{form});
+            console.log("회원 가입에 성공했습니다.",response.data);
+        } catch (e) {
+            console.e("회원 가입 실패",e.response?.data || e.message);
+        }
+    }
 
     return(
         <div>
-            <SignUpForm/>
+            <SignUpForm onSignUp={onSignUp}/>
         </div>
     )
 };

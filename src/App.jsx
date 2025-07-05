2a1f335c0f93f6a8fa6bc71b6afbf8c6625b40e6
kim Gun ho <vlfh0002@gmail.com>
2025-07-05 15:24:40 +0900

    router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductListPage from './pages/user/product/ProductListPage';
 import NotFoundPage from './pages/user/product/NotFoundPage';
 import MyReviewPage from './pages/user/review/MyReviewPage';
+import AdminReviewPage from './pages/admin/review/AdminReviewPage';
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/products" element={<ProductListPage />} />
         <Route path="*" element={<NotFoundPage />} /> {/* 옵션 */}
         <Route path="/mypage/review" element = {<MyReviewPage/>} />
+        <Route path='/admin/review' element = {<AdminReviewPage/>}/>
       </Routes>
     </Router>
   )

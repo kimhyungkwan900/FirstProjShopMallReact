1167bf68f5a21247d411f8969b9fd0743b01a97a
김형관 <bearson0115@gmail.com>
2025-07-09 07:06:47 +0900

    2025_07_09_0706

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import productRoutes from './user/product/ProductRoutes';
 import loginRoutes from './member/login/LoginRouter';
+import reviewRoutes from './user/review/ReviewRouter';
 import NotFoundPage from '../pages/user/product/NotFoundPage';
 import MainPage from '../pages/MainPage';
 
 const router = createBrowserRouter([
   ...productRoutes,
   ...loginRoutes,
+  ...reviewRoutes,
   {
     path: '/',
     element: <MainPage  />

b099b7f22d2ed0e2df700cc916b3b2b83db0aecc
김형관 <bearson0115@gmail.com>
2025-07-10 06:57:35 +0900

    2025_07_10_0700

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -8,6 +8,7 @@ import productManageRoutes from './admin/productmanage/ProductManageRoutes';
 import orderManageRoutes from './admin/ordermanage/OrderManageRoutes';
 import myPageRouter from './member/mypage/myPageRouter';
 import CartRouter from './cart/CartRouter';
+import CartButton from '../component/user/cart/CartButton';
 import OrderRouter from './order/OrderRouter';
 import FaqRouter from './admin/faq/FaqRouter';
 

0b0d522f393db99d6592348d28b26451290d91ab
jiwooa <wldn5913@gmail.com>
2025-07-09 10:31:01 +0900

    변경 사항 저장

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -6,7 +6,7 @@ import MainPage from '../pages/MainPage';
 import ReviewRouter from './user/review/ReviewRouter';
 import productManageRoutes from './admin/productmanage/ProductManageRoutes';
 import orderManageRoutes from './admin/ordermanage/OrderManageRoutes';
-import CartRouter from './cart/CartRouter';
+import CartButton from '../component/user/cart/CartButton';
 
 const router = createBrowserRouter([
   ...orderManageRoutes,
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
   ...productRoutes,
   ...loginRoutes,
   ...ReviewRouter,
+  ...CartButton,
   {
     path: '/',
     element: <MainPage  />

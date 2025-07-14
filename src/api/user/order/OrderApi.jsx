4f5148d10b65938e578a923325c825b40c43f39a
jiwooa <wldn5913@gmail.com>
2025-07-14 17:48:15 +0900

    임시 저장

--- src/api/user/order/OrderApi.jsx
+++ src/api/user/order/OrderApi.jsx
@@ -10,25 +10,10 @@ export const createOrder = async (memberId, orderData) => {
   return axiosInstance.post(`/api/orders/${memberId}`, orderData);
 };
 
-//[2] 결제 성공 처리 API
-export const completeOrder = async (orderId) => {
-  return axiosInstance.post(`/api/orders/${orderId}/complete`);
-};
-
-//[3] 결제 실패 처리 API
-export const failedOrder = async (orderId) => {
-  return axiosInstance.post(`/api/orders/${orderId}/failed`);
-};
 
 // [4] 배송 요청사항 저장 API
 export const saveDeliveryRequest = async (orderData) => {
   return axiosInstance.post(`/api/orders/deliveryRequest`, orderData);
 };
 
-//[5] 결제 처리 API
-export const payOrder = async (orderId, paymentToken) => {
-  return axiosInstance.post(`/api/orders/${orderId}/pay`, null, {
-    params: { paymentToken },
-  });
-};
 
